c72ff4c3e68c5c08a135a15d70a89e245783c022
emrhssla <emrhssla@gmail.com>
2021-06-03 15:43:45 +0900

    모범답안

--- Programmers/Level1/test.c
+++ Programmers/Level1/test.c
@@ -1,64 +1,29 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-# define SWAP(x, y, temp) ( (temp)=(x), (x)=(y), (y)=(temp) )
-// numbers_len은 배열 numbers의 길이입니다.
+#define SWAP(a,b) {int c = a; a=b; b=c;} 
 int* solution(int numbers[], size_t numbers_len) {
-    int i =0,k = 0, j;
-    int h = 0;
-    int min=0, temp;
-    // 순서관계없이 n개 중 2개를 선택 nC2 => (n*n-1)/2
-    int len = (numbers_len)*(numbers_len-1)/2;
-    int* answer = (int*)malloc(sizeof(int)*len);
-    int* arr = (int*)malloc(sizeof(int)*len);
-    //
-    for(i;i<numbers_len-1;i++){
-        for(j=i+1;j<numbers_len;j++,k++){
-            answer[k]=numbers[i]+numbers[j];
-            printf("answer =  %d + %d = %d\n",numbers[i],numbers[j],answer[k]);
+    int* answer = (int*)malloc(sizeof(int)*200);
+    int cnt=0;
+    for (int i=0; i<numbers_len-1; i++){
+        for (int j=i+1; j<numbers_len; j++){
+            int tmp = numbers[i]+numbers[j];
+            int flag = 1;
+	    //같은 숫자 체크
+            for (int k=0; k<cnt; k++){
+                if (answer[k]==tmp)
+                    flag = 0;
+            }
+            if (flag ==1)
+                answer[cnt++]=tmp;
         }
     }
-    for(i=0;i<len;i++)
-        printf(" %d, ",answer[i]);
-    printf("\n ------------------ \n");
-    //큰순서로 나열하기 (선택정렬)
-    for(i=0,h=0; i<k-1; i++){
-        min = i;
-        for(j=i+1; j<k; j++){
-            if(answer[j]<answer[min])
-                min = j;
-        }
-        if(i != min){
-            SWAP(answer[i], answer[min], temp);
-        }
-        // 새로운 배열에 넣는다./ 중복제거를 위해
-        arr[h]=answer[i];
-        printf("%d번째 %d 삽입 ",h+1,arr[h]);
-        //중복제거
-        if(arr[h]!=arr[h-1])
-        {
-            h++;
+//버블정렬
+    for (int i=0; i<cnt; i++){
+        for (int j=0; j<cnt-1-i; j++){
+            if (answer[j]>answer[j+1])
+                SWAP(answer[j],answer[j+1]);
         }
     }
-    printf("arr[%d]번쨰 : %d, answer[%d]번째: %d",h,arr[h],i,answer[i]);
-    //마지막 배열 숫자 삽입
-    arr[h]=answer[i];
-    printf("arr[h+1] : %d",arr[h]);
-    for(i=0;i<len;i++)
-        printf(" %d, ",answer[i]);
-    printf("\n ------------------ \n");
-    for(i=0;i<h;i++)
-        printf(" %d, ",arr[i]);
-    /*
-    //중복제거
-    for(i=0,h=0;i<k;i++,h++){
-        if(answer[i-1]==answer[i]){
-            h--;
-            continue;
-        }
-        arr[h]=answer[i];
-    }
-    */
-        
-    return arr;
+    return answer;
 }

8b39d0bc35affeb30932cea77a0859728fe42f9c
emrhssla <emrhssla@gmail.com>
2021-06-11 15:33:02 +0900

    자릿수 더하기 ver 정수를 문자열로 변환

--- Programmers/Level1/test.c
+++ Programmers/Level1/test.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
 
 int solution(int n) {
     int answer = 0;
+    char* str=(char*)malloc(sizeof(char)*10);
+    sprintf(str,"%d",n);
     int i;
     
-    for(i=1;n/i>=10;i*=10){}
-    while(i>=1)
+    for(i=0;(int)str[i]>47 && (int)str[i]<59;i++)
     {
-        answer += ( n / i );
-        n -= n / i * i ;
-        i/=10;
+        answer+=( (int)str[i]-48 );
     }
-    
-        
-    
-    
+    printf("%d",(int)str[0]-48);
     
     return answer;
 }

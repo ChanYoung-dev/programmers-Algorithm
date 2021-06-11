15545ef57d3964a91350d3112eff1471cd1b696f
emrhssla <emrhssla@gmail.com>
2021-06-11 15:13:47 +0900

    자릿수 더하기

--- Programmers/Level1/test.c
+++ Programmers/Level1/test.c
@@ -1,6 +1,22 @@
-void swap_a(int *a,int *b){
-    int tmp = 0;
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    int i;
+    
+    for(i=1;n/i>=10;i*=10){}
+    while(i>=1)
+    {
+        answer += ( n / i );
+        n -= n / i * i ;
+        i/=10;
+    }
+    
+        
+    
+    
+    
+    return answer;
 }

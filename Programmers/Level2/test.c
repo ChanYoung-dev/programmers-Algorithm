8f8448ba25e1ed31d9abe6b0baeb73ac3c654f73
emrhssla <emrhssla@gmail.com>
2021-06-23 22:19:54 +0900

    strcat + 수박수박 + 한글형 배열

--- Programmers/Level2/test.c
+++ Programmers/Level2/test.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include <stdlib.h>
 
-int solution(int n) {
-    int answer = 0;
-    while(n){
-        answer += n%10;
-        n /=10;
+char* solution(int n) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    
+    
+    char* su="수";
+    char* bak="박";
+    
+    printf("%d",strlen(su));
+    
+    char* answer = (char*)malloc(sizeof(char)*n*3); 
+    //strcpy(answer, ""); //answer 초기화
+    for(int i=0;i<n;i++){
+        if(i%2==0)
+            strcat(answer,su);
+        else
+            strcat(answer,bak);
     }
     return answer;
 }

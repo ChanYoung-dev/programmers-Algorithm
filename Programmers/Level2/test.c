d077bda83fff2ac3e6ac7d8df6d6fbc93f631fae
emrhssla <emrhssla@gmail.com>
2021-06-24 17:44:04 +0900

    소수형

--- Programmers/Level2/test.c
+++ Programmers/Level2/test.c
@@ -1,36 +1,10 @@
-#include<memory.h> //#include<string.h>
-#include<stdio.h>
- 
-void printAll(int* pArr, int len)
-{
-    for (int i = 0; i < len; ++i)
-    {
-        printf("%d ", *(pArr + i));
+bool checkSoSu(int n) {
+    int i=n-1;
+    while(i!=1) {
+        if(n%i == 0) {
+            return false;
+        } else i--;
     }
- 
-    printf("\n\n");
-}
- 
-int solution(void)
-{
-    // 배열 초기화 for 문 이용
-    int arr1[20];
-    /*
-    for (int i = 0; i < 20; ++i)
-    {
-        arr1[i] = 0;
-    }
- */
-    printf("for    : ");
-    printAll(arr1, 20);
- 
- 
-    // 배열 초기화 memset 함수이용
-    int arr2[20];
-    memset(arr2, 0, 20 * sizeof(int));
- 
-    printf("memset : ");
-    printAll(arr2, 20);
- 
-    return 0;
+
+    return true;
 }

9c02ee51a53a6317f1e253773f16ac86c22628ef
emrhssla <emrhssla@gmail.com>
2021-06-25 22:54:47 +0900

    최대공약수구하 오류 버전

--- Programmers/Level2/test.c
+++ Programmers/Level2/test.c
@@ -1,10 +1,24 @@
-bool checkSoSu(int n) {
-    int i=n-1;
-    while(i!=1) {
-        if(n%i == 0) {
-            return false;
-        } else i--;
-    }
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-    return true;
+long long solution(long long w, long long h) {
+    long long answer = 1;
+    long long maxCheck=0;
+    long long i;
+    long long num;
+    
+    //소수 구하기
+    num= ( w>h ) ? h : w;
+    for( i=num ; i>0; i--){
+        if( ( (w%i) ==0 ) && ( (h%i) ==0 ) ){
+            maxCheck=i;
+            break;
+        }
+    }
+    
+    answer = w*h-( maxCheck * ( (w/maxCheck) + (h/maxCheck)-1 ) );
+    //answer = (w*h)-(w+h-maxCheck);
+    
+    return answer;
 }

738f111e28d1a536221594db9627c53809f5c33e
emrhssla <emrhssla@gmail.com>
2021-06-25 22:55:34 +0900

    최대공약수구하 정식버전

--- Programmers/Level2/test.c
+++ Programmers/Level2/test.c
@@ -8,7 +8,7 @@ long long solution(long long w, long long h) {
     long long i;
     long long num;
     
-    //소수 구하기
+    //w와 h의 최대공약수 구하기 구하기
     num= ( w>h ) ? h : w;
     for( i=num ; i>0; i--){
         if( ( (w%i) ==0 ) && ( (h%i) ==0 ) ){

f381b2e9bea8e45e53a8e513e328cd089b8b1739
emrhssla <emrhssla@gmail.com>
2021-07-06 12:11:25 +0900

    2개이하로 다른비트 ,정답풀이

--- Programmers/Level2/test.c
+++ Programmers/Level2/test.c
@@ -1,45 +1,43 @@
+/*
+끝 비트에서 0이 몇번째자리에서 나오냐가 핵심이다.
+끝자리가 11110 :  
+-> +1을 할경우   정답
+끝자리가 11101 :
+-> +1을 할경우 11110 정답
+끝자리가       11011
+-> +1을 할경우 11100 다른게 3개이므로 오답
+-> +2을 할경우 11101 다른게 2개이므로 정답
+끝자리가       10111
+-> +1을 할경우 11000 다른게 4개이므로 오답
+-> +2을 할경우 11001 다른게 3개이므로 오답
+-> +4을 할경우 11011 다른게 2개이므로 정답
+끝자리가       101111
+-> +1을 할경우 110000 다른게 5개이므로 오답
+-> +2을 할경우 110001 다른게 4개이므로 오답
+-> +4을 할경우 110011 다른게 3개이므로 정답
+-> +8을 할경우 110111 다른게 2개이므로 정답
+기존값에 1,2,4,8,16 ... 2의제곱근만 더해주면된다.
+*/
+
+
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
-// numbers_len은 배열 numbers의 길이입니다.
 long long* solution(long long numbers[], size_t numbers_len) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int arr[50];
-    int k;
     long long* answer = (long long*)malloc(numbers_len*sizeof(long long));
-    //printf("%lld",numbers[0]);
     int i,m,j;
-    int result1,result,testing;
-    int sum=1;
-    int max;
-    long long testnum;
+    long long sum=1;
     for(m=0;m<numbers_len;m++){
         sum=1;
-        testnum=numbers[m];
-        printf("%lld 검사시작 -\n",testnum);
-        printf("2진법 계산 :");
-        for (int x = 7; x >= 0; --x) {
-            int result = testnum >> x & 1;
-            printf(" %d", result);
-        }
-        printf("\n");
-        
-        for (i = 0; i<=50; i++) { 
-                if(1 != (testnum >> i & 1))
+        //0의 자리가 i번째에서 뜨는가?
+        for (i = 0; i<=50; i++) 
+                if(1 != (numbers[m] >> i & 1))
                     break;
-        }
-        printf("i :%d,",i);
-        
-        for(j=1;j<i;j++){
+        // 기존값 + 2의 (i-j)제곱근
+        for(j=1;j<i;j++)
             sum*=2;
-        }
-        printf("sum : %d \n",sum);
-        answer[m]=testnum+sum;  
-        
-        printf("검사결과 answer[%d]:%lld \n",m,answer[m]);
-        printf("-----------------------------\n");
+        answer[m]=numbers[m]+sum;  
     }
-    
     return answer;
 }

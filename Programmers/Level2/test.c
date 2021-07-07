4bcbed491a234be139dd5c55f0da5a5328327ed0
emrhssla <emrhssla@gmail.com>
2021-07-07 15:06:10 +0900

    이진 변환 반복하기

--- Programmers/Level2/test.c
+++ Programmers/Level2/test.c
@@ -1,43 +1,53 @@
-/*
-끝 비트에서 0이 몇번째자리에서 나오냐가 핵심이다.
-끝자리가 11110 :  
--> +1을 할경우   정답
-끝자리가 11101 :
--> +1을 할경우 11110 정답
-끝자리가       11011
--> +1을 할경우 11100 다른게 3개이므로 오답
--> +2을 할경우 11101 다른게 2개이므로 정답
-끝자리가       10111
--> +1을 할경우 11000 다른게 4개이므로 오답
--> +2을 할경우 11001 다른게 3개이므로 오답
--> +4을 할경우 11011 다른게 2개이므로 정답
-끝자리가       101111
--> +1을 할경우 110000 다른게 5개이므로 오답
--> +2을 할경우 110001 다른게 4개이므로 오답
--> +4을 할경우 110011 다른게 3개이므로 정답
--> +8을 할경우 110111 다른게 2개이므로 정답
-기존값에 1,2,4,8,16 ... 2의제곱근만 더해주면된다.
-*/
-
-
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-long long* solution(long long numbers[], size_t numbers_len) {
-    long long* answer = (long long*)malloc(numbers_len*sizeof(long long));
-    int i,m,j;
-    long long sum=1;
-    for(m=0;m<numbers_len;m++){
-        sum=1;
-        //0의 자리가 i번째에서 뜨는가?
-        for (i = 0; i<=50; i++) 
-                if(1 != (numbers[m] >> i & 1))
-                    break;
-        // 기존값 + 2의 (i-j)제곱근
-        for(j=1;j<i;j++)
-            sum*=2;
-        answer[m]=numbers[m]+sum;  
+#include <string.h>
+
+int* solution(const char* s) {
+    char* arr=(char*)malloc(strlen(s));
+    int* answer = (int*)malloc(sizeof(int) * 2);
+    int i,j,num,size;
+    int zero_count=0;
+    int zero_total=0;
+    strcpy(arr,s); // 내용을 복사하는 것
+    //arr=s; ->주소를 복사 하는 것
+    int len=strlen(s);
+    //num은 회차이다.
+    for(num=0; len!=1; num++){
+        int *ten_to_two= (int*)calloc(size,sizeof(int));
+        size=1;
+        zero_count=0;
+        //0의 갯수 Count
+        for(i=0;arr[i]!='\0';i++)
+            if(arr[i]=='0')
+                zero_count++;
+        //전체 0의 갯수
+        zero_total+=zero_count;
+        //len = 기존 배열길이 - 0의 원소 갯수
+        len= i-zero_count;
+        //len10진수를 2진수로 바꾸기
+        if(len!=1)
+        {
+            // 배열에 원소 넣기 ex) 11 => 1 1 0 1 (2로 나누고 나머지 값) 
+            for (i = 0; len != 0; i++) {
+                ten_to_two[i] = len % 2;
+                len /= 2;
+                if (len != 0) {
+                    size++;
+                    ten_to_two = realloc(ten_to_two, sizeof(int)*size);
+                }
+            }
+            // 배열 원소 거꾸로 출력 , 결과값을 arr배열에 저장
+            for (i = size - 1,j=0; i >= 0; i--,j++) {
+                arr[j]=ten_to_two[i]+'0';
+            }
+            arr[j]='\0';
+            free(ten_to_two);
+        }
+
     }
+    answer[0]=num;
+    answer[1]=zero_total;
+    
     return answer;
 }
